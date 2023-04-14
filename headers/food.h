8685e6da1a515a2bb6e68267cabd981f6dafec5b
Wendy <wendyhaw9@gmail.com>
2023-04-14 14:55:25 -0700

    Adam's changes

--- headers/food.h
+++ headers/food.h
@@ -6,6 +6,8 @@
 // This class is the parent class for all of the things Pac Man can eat except for the blue Ghosts.
 // This includes the fruit, the orbs, and the orbs that turn the ghosts blue.
 class Eatable : public GameObject {
+    position pos;
+
 };
 
 #endif
---
+++ headers/pacman.h
@@ -0,0 +1,12 @@
+#ifndef PACMAN_H
+#define PACMAN_H
+
+class Pacman : public Sprite {
+
+void colliion(Node node)
+{
+    if 
+}
+}
+
+#endif
